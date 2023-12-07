314083725644bf4537a9f4d5f53becf5e52f82ed
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-12-07 15:58:37 +0000

    0x00. AirBnB clone - The console

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for managing amenity objects.
+
+    Attributes:
+        name (str) : The name of the amenity.
+    """
+
+    name = ""
