9d64eb9bf72e8600fb195958b216f79705c0a97b
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-12-09 13:55:00 +0000

    0x00. AirBnB clone - The console

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module creates a User class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for managing city objects
+
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
+    state_id = ""
+    name = ""
